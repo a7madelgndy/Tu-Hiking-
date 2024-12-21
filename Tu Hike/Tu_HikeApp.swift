f3a1ceb18ff110e1928d4fe9ebd8bb62002ee87b
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-21 03:07:48 +0200

    Initial Commit

---
+++ Tu Hike/Tu_HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_HikeApp.swift
+//  Tu Hike
+//
+//  Created by Ahmed El Gndy on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
